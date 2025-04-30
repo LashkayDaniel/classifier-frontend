a4b8aaf0bb6f696a7719d26673929d06b3d90906
lashkaydaniel <lashkay74y@gmail.com>
2025-04-30 15:18:42 +0300

    add loading spinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const { size = 4 } = defineProps<{
+  size?: number
+}>()
+
+const customSizeClass = computed(() => `size-${size}`)
+</script>
+
+<template>
+  <div class="rounded-full border-2 border-x-transparent animate-spin"
+       :class="customSizeClass" />
+</template>
