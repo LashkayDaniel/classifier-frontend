5bf0eaba55c2d6f1b22dfcfc5583bafc4523fe94
lashkaydaniel <lashkay74y@gmail.com>
2025-05-01 14:07:38 +0300

    create accordion component

---
+++ src/components/AccordionItem.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  title: string,
+}>()
+
+const showContent = ref<boolean>(false)
+
+const toggleShow = () => {
+  showContent.value = !showContent.value
+}
+
+</script>
+
+<template>
+  <div class="p-2 bg-stone-400/50 m-2">
+    <div
+      class="flex justify-between border-stone-400 p-4 hover:bg-stone-400/20 transition-all duration-400 hover:rounded-lg cursor-pointer"
+      :class="{'border-b':showContent}"
+      @click.prevent="toggleShow">
+      <h2 class="font-semibold text-stone-700">{{ title }}</h2>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+           stroke="currentColor" class="size-6 stroke-stone-500"
+           :class="{'rotate-180': showContent}">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+      </svg>
+    </div>
+
+    <Transition
+      name="fade"
+      enter-active-class="transition-all duration-500"
+      leave-active-class="transition-all duration-500"
+    >
+      <div v-show="showContent" class="mt-2">
+        <slot />
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<style scoped>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  max-height: 0;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+  max-height: 500px;
+}
+</style>
