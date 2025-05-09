fb670109f1214f08c2f84da4c412a67bbfcac6c8
lashkaydaniel <lashkay74y@gmail.com>
2025-05-09 10:40:20 +0300

    add not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ClassifyView from '@/views/ClassifyView.vue'
 import { useDatasetStore } from '@/stores/dataset.ts'
+import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
     {
       path: '/',
       name: 'home',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const pageLoaded = ref<boolean>(false)
+
+onMounted(() => {
+  pageLoaded.value = true
+})
+</script>
+
+<template>
+  <Transition name="fade-in">
+    <section v-if="pageLoaded"
+             class="relative bg-stone-400/60 border border-stone-400/30 text-center text-stone-800 p-6 mx-auto w-fit mt-20 rounded-xl min-w-md shadow-2xl overflow-hidden"
+    >
+      <div
+        class="absolute -top-12 -left-12 size-32 bg-purple-300/30 rounded-full blur-2xl"></div>
+      <div
+        class="absolute -bottom-12 -right-12 size-32 bg-amber-200/30 rounded-full blur-2xl"></div>
+
+      <h1 class="text-7xl font-bold tracking-widest drop-shadow-lg">404</h1>
+      <p class="mb-6 text-xl font-medium">Page not found</p>
+
+      <RouterLink
+        to="/"
+        class="inline-block bg-black/80 px-6 py-2 text-gray-200 rounded-lg hover:bg-black transition-all duration-300 shadow-md"
+      >
+        Go Home
+      </RouterLink>
+    </section>
+  </Transition>
+</template>
+
+
+<style scoped>
+.fade-in-enter-active,
+.fade-in-leave-active {
+  transition: opacity 0.5s ease, transform 0.5s ease;
+}
+
+.fade-in-enter-from,
+.fade-in-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+
+.fade-in-enter-to,
+.fade-in-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
